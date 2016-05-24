76a6c57a7daa1db3a5b3f2ad85db3250ae2fa088
kreo <hi@kreo.me>
2016-05-24 17:58:26 +0200

    reafactor bemit selector to attributes

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 module.exports = function(opts) {
-  var implicit = (opts && opts.implicit == false) ? false : true;
+  var implicit = (opts && opts.implicit === false) ? false : true;
 
   return function(style){
     style.include(__dirname);
     if (implicit) { style.import("nixin/source/styles"); }
-  }
+  };
 
-}
+};
