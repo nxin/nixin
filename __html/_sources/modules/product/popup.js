dac6c3d1ce55c601a6c30f4b61f0a375d62787db
kreo <hi@kreo.me>
2016-06-24 16:31:13 +0200

    u __html

--- __html/_sources/modules/product/popup.js
+++ __html/_sources/modules/product/popup.js
@@ -276,7 +276,83 @@ $(function() {
 
     $('.product').on('click', ".js-button--detail", function (e) {
         e.preventDefault();
+
         open('.js-popup--detail');
+
+        $carouselPopup = $(".js-carousel--popup");
+
+        $carouselPopup.find(".js-carousel__items").slick({
+            autoplay: false,
+            autoplaySpeed: 2000,
+            dots: true,
+            infinite: false,
+            speed: 200,
+            slidesToShow: 9,
+            slidesToScroll: 9,
+            variableWidth: false,
+            lazyLoad: 'ondemand',
+            cssEase: 'ease-in-out',
+            mobileFirst: true,
+            centerMode: false,
+            prevArrow: $carouselPopup.find(".js-carousel__prev"),
+            nextArrow: $carouselPopup.find(".js-carousel__next"),
+            responsive: [
+                {
+                    breakpoint: 0,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                        dots: false
+                    }
+                },
+                {
+                    breakpoint: 321,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                        dots: false
+                    }
+                },
+                {
+                    breakpoint: 481,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 2,
+                        dots: false
+                    }
+                },
+                {
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 3,
+                        dots: false
+                    }
+                },
+                {
+                    breakpoint: 993,
+                    settings: {
+                        slidesToShow: 4,
+                        slidesToScroll: 4
+                    }
+                },
+                {
+                    breakpoint: 1281,
+                    settings: {
+                        slidesToShow: 5,
+                        slidesToScroll: 5
+                    }
+                },
+                {
+                    breakpoint: 1921,
+                    settings: {
+                        slidesToShow: 5,
+                        slidesToScroll: 5
+                    }
+                }
+            ]
+        });
+
     });
 
 
